2c7c87808913a359911d0a65e3bd15355c7550a4
frnore <fcardoso@disid.com>
2019-01-29 13:25:24 +0100

    Create test for service

--- src/app/core/service/propietario.service.spec.ts
+++ src/app/core/service/propietario.service.spec.ts
@@ -1,12 +1,52 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PropietarioService } from './propietario.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Owner } from 'src/app/shared/models/propietario';
+import { environment } from 'src/environments/environment';
+import * as moment from 'moment';
 
 describe('PropietarioService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  // tslint:disable-next-line:prefer-const
+  let service: PropietarioService;
+  // tslint:disable-next-line:prefer-const
+  let httpTestingController: HttpTestingController;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+
+    // Inject the http service and test controller for each test
+    service = TestBed.get(PropietarioService);
+    httpTestingController = TestBed.get(HttpTestingController);
+
+  });
 
   it('should be created', () => {
-    const service: PropietarioService = TestBed.get(PropietarioService);
     expect(service).toBeTruthy();
+  });
+
+  it('should get the mascotas list successful', () => {
+    const testData: Owner[] = [new Owner({
+      'id': 40, 'firstName': 'Prueba', 'lastName': 'McTest',
+      'birthDay': moment(`2018-12-11`), 'address': 'Calle Falsa 123',
+      'city': 'Madrid', 'telephone': '600111222'
+    }),
+    new Owner({
+      'id': 40, 'firstName': 'Paco', 'lastName': 'Font',
+      'birthDay': moment(`2011-06-10`), 'address': 'Calle de Las Torres 6',
+      'city': 'Sedavi', 'telephone': '699881772'
+    })];
+
+    service.listAll().subscribe(data => {
+      expect(data[0]).toEqual(testData[0]);
+      expect(data.length).toBe(2);
+    });
+
+    const endpoint = environment.apiURL + '/owners';
+    const req = httpTestingController.expectOne(endpoint, 'call to api');
+    expect(req.request.method).toEqual('GET');
+    req.flush(testData);
+    httpTestingController.verify();
   });
 });
