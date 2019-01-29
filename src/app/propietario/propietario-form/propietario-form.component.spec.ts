8f40625d49fd3d10576f5275a9c9a02968c1ffd4
frnore <fcardoso@disid.com>
2019-01-29 19:35:33 +0100

    Create test for Form component

--- src/app/propietario/propietario-form/propietario-form.component.spec.ts
+++ src/app/propietario/propietario-form/propietario-form.component.spec.ts
@@ -1,16 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PropietarioFormComponent } from './propietario-form.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { createTranslateLoader } from 'src/app/core/core.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatInputModule, MatDatepickerModule, DateAdapter, MatNativeDateModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('PropietarioFormComponent', () => {
+
+fdescribe('PropietarioFormComponent', () => {
   let component: PropietarioFormComponent;
   let fixture: ComponentFixture<PropietarioFormComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PropietarioFormComponent ]
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: (http: HttpClient) => (createTranslateLoader),
+            deps: [HttpClient]
+          }
+        }),
+        HttpClientTestingModule,
+        HttpClientModule,
+        MatInputModule,
+        FormsModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        RouterTestingModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        MatDatepickerModule
+      ],
+      declarations: [PropietarioFormComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +48,23 @@ describe('PropietarioFormComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('component HTML form should have all fields', () => {
+    const formElement: HTMLElement = fixture.nativeElement;
+    const firstNameField = formElement.querySelector('input[name="firstName"]');
+    expect(firstNameField).toBeTruthy();
+    const lastNameField = formElement.querySelector('input[name="lastName"]');
+    expect(lastNameField).toBeTruthy();
+    const addressField = formElement.querySelector('input[name="address"]');
+    expect(addressField).toBeTruthy();
+    const cityField = formElement.querySelector('input[name="city"]');
+    expect(cityField).toBeTruthy();
+    const telephoneField = formElement.querySelector('input[name="telephone"]');
+    expect(telephoneField).toBeTruthy();
+    const birthDayField = formElement.querySelector('input[name="birthDay"]');
+    expect(birthDayField).toBeTruthy();
   });
 });
